50e4017d35da5d4cf1f34a483b293dcd8399812e
zottaa <zottaa691@gmail.com>
2023-11-19 17:35:47 +0700

    finished task008

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -2,10 +2,34 @@ package ru.easycode.zerotoheroandroidtdd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.core.view.isVisible
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var titleTextView: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val hideButton = findViewById<Button>(R.id.hideButton)
+        titleTextView = findViewById(R.id.titleTextView)
+
+        if (savedInstanceState != null) {
+            if (savedInstanceState.getBoolean("isHide", false)) {
+                titleTextView.visibility = TextView.GONE
+            }
+        }
+
+        hideButton.setOnClickListener {
+            titleTextView.visibility = TextView.GONE
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putBoolean("isHide", !titleTextView.isVisible)
+
+        super.onSaveInstanceState(outState)
     }
 }
