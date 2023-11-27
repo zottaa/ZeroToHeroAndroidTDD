2ba8d14fc20cf226e6940c39d46b898f16cd8dba
zottaa <zottaa691@gmail.com>
2023-11-27 18:12:08 +0700

    finished task014

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -2,10 +2,31 @@ package ru.easycode.zerotoheroandroidtdd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.View
+import android.widget.Button
+import android.widget.ProgressBar
+import android.widget.TextView
+import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val progressBar = findViewById<ProgressBar>(R.id.progressBar)
+        val textView = findViewById<TextView>(R.id.titleTextView)
+        val button = findViewById<Button>(R.id.actionButton)
+
+        button.setOnClickListener {
+            it.isEnabled = false
+            progressBar.visibility = View.VISIBLE
+            Handler(Looper.getMainLooper()).postDelayed({
+                textView.visibility = View.VISIBLE
+                it.isEnabled = true
+                progressBar.visibility = View.INVISIBLE
+            }, 2000)
+        }
     }
 }
