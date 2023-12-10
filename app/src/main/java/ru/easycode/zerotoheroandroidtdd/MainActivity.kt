2ebe8572ce3262b740133a68b36f44753c189019
zottaa <zottaa691@gmail.com>
2023-12-11 03:37:56 +0700

    finished task020

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -2,10 +2,16 @@ package ru.easycode.zerotoheroandroidtdd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ru.easycode.zerotoheroandroidtdd.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.actionButton.setOnClickListener {
+            UiState.ShowText.apply(binding.titleTextView, binding.inputEditText)
+        }
     }
 }
---
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/UiState.kt
@@ -0,0 +1,17 @@
+package ru.easycode.zerotoheroandroidtdd
+
+import android.widget.TextView
+import com.google.android.material.textfield.TextInputEditText
+import java.io.Serializable
+
+interface UiState : Serializable{
+
+    fun apply(textView: TextView, editText: TextInputEditText)
+
+    object ShowText : UiState {
+        override fun apply(textView: TextView, editText: TextInputEditText) {
+            textView.text = editText.text
+            editText.text?.clear()
+        }
+    }
+}
